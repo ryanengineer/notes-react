230f6217b603d126158fdd1e46427f15851e3c37
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2020-02-21 09:46:59 +0500

    Updates

---
+++ precache-manifest.fe1d77e23f40b40d840fbf4345230652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe889ecd8829181b667",
+    "url": "/notes-react/static/js/runtime~main.9fe889ec.js"
+  },
+  {
+    "revision": "c30b27f1d9d9edcb9673",
+    "url": "/notes-react/static/js/main.c30b27f1.chunk.js"
+  },
+  {
+    "revision": "a1fa9026ffae8d16d332",
+    "url": "/notes-react/static/js/2.a1fa9026.chunk.js"
+  },
+  {
+    "revision": "a1fa9026ffae8d16d332",
+    "url": "/notes-react/static/css/2.c1828ce2.chunk.css"
+  },
+  {
+    "revision": "e54812a2674e669081cb9f3604f06797",
+    "url": "/notes-react/index.html"
+  }
+];
